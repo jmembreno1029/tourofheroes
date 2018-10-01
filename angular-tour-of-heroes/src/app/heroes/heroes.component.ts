0b8673c8ad4402eee19aa61a0a8d01a4d3709df6
Joshua Membreno <membrenojoshua@gmail.com>
2018-09-30 23:20:08 -0500

    added dashboard component and routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,11 +7,9 @@ import { HeroService } from "../hero.service";
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -19,13 +17,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);  
-  } 
-  
-
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
